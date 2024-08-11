818f50c6b40c2444695959c30c24353472ed4454
shwncho <shwncho4@gmail.com>
2024-08-11 15:31:03 +0900

    properties

---
+++ src/main/kotlin/Properties/Properties.kt
@@ -0,0 +1,10 @@
+package Properties
+
+class PropertyExample() {
+    var counter = 0
+    var propertyWithCounter: Int? = null
+        set(value){
+            field = value
+            counter++
+        }
+}
